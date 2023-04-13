cca57f9e10b798fa4d6f23ad9bd84d5a92b9123f
Peter-Paul van Gemerden <mail@ppvg.nl>
2023-04-13 10:26:10 +0200

    chore: run formatter

--- scripts/build-css.js
+++ scripts/build-css.js
@@ -4,9 +4,12 @@ const fs = require("fs").promises;
 const path = require("path");
 const sass = require("sass");
 
-const includePaths = [path.resolve(__dirname, "../scss"), path.resolve(__dirname, "../node_modules")];
+const includePaths = [
+  path.resolve(__dirname, "../scss"),
+  path.resolve(__dirname, "../node_modules"),
+];
 const outDir = path.resolve(__dirname, "../css");
-const outFile = 'main.css';
+const outFile = "main.css";
 
 const promises = [];
 
@@ -16,19 +19,16 @@ const data = `
 
   @import "main";
 `;
-const result = sass.compileString(
-  data,
-  {
-    style: "compressed",
-    sourceMap: true,
-    loadPaths: includePaths
-  }
-);
+const result = sass.compileString(data, {
+  style: "compressed",
+  sourceMap: true,
+  loadPaths: includePaths,
+});
 
 promises.push(fs.mkdir(outDir, { recursive: true }));
 promises.push(fs.writeFile(path.resolve(outDir, outFile), result.css.toString()));
 
 Promise.all(promises).catch((error) => {
-	console.error(error);
-	process.exit(1);
+  console.error(error);
+  process.exit(1);
 });
