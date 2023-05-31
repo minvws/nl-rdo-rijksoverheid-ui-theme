2fdb7db2ca7bfc00a2e786ca2b33ee9a04ccfcd3
Rick Lambrechts <rick@rl-webdiensten.nl>
2023-05-31 16:19:16 +0200

    Capture screenshots (#153)
    * Capture screenshots
    
    * Removed console log
    
    * Add PUPPETEER_SKIP_DOWNLOAD
    
    * Add rebuild puppeteer to install chrome
    
    * Capture png screenshots
    
    * Update job name in visual testing workflow
    
    * Added reg-cli screenshot compare tool
    
    * Use waitUntil load
    
    * Run reg-cli in workflow
    
    * Remove reg-cli from package json and install in workflow
    
    * Use different steps to capture and do visual testing
    
    * Remove cache and registry from setup node in compare screenshots
    
    * Remove screenshots artifact after visual comparing
    
    * Push screenshots to branch after merge
    
    * Update on section in github workflow
    
    * Update npm workflow
    
    * Add concurrency options
    
    * Update workflow names
    
    * Use reusable workflow
    
    * Option to specify capture screenshots output folder
    
    * Custom github action
    
    * Fix needed workflow
    
    * Update workflow and action
    
    * Update workflow and action
    
    * Add shell in action
    
    * Update workflow
    
    * Only run visual testing on pull request
    
    * Update workflow
    
    * Configurable html directory and checkout ref in action
    
    * Add shell
    
    * Update github output script
    
    * Update version
    
    * Continue on error

---
+++ tests/capture-screenshots.js
@@ -0,0 +1,60 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+const { readdir, mkdir } = require('fs').promises;
+
+async function getHtmlFiles(dir) {
+  const files = await readdir(dir, { recursive: true });
+
+  return files.filter((file) => {
+    return path.extname(file).toLowerCase() === '.html';
+  });
+}
+
+async function screenshotWebpage(browser, filePath, screenshotPath) {
+  let page = await browser.newPage();
+
+  // Set screen size
+  await page.setViewport({width: 1080, height: 1024});
+
+  await page.goto(`file://${filePath}`, {waitUntil: 'load'});
+  await page.screenshot({path: screenshotPath, fullPage: true, captureBeyondViewport: false});
+
+  await page.close();
+}
+
+function getScreenshotFileName(htmlFilePath) {
+  return htmlFilePath.replaceAll(path.sep, '-').replace(/\.html$/, '.png');
+}
+async function main(screenshotDirectory, htmlDirectory) {
+  // Create screenshot directory
+  await mkdir(screenshotDirectory, { recursive: true })
+
+  // Get all html files
+  const htmlFiles = await getHtmlFiles(htmlDirectory);
+
+  // Launch browser
+  const browser = await puppeteer.launch({headless: true});
+
+  // Loop through html files and capture screenshot
+  for (const htmlFile of htmlFiles) {
+    const filePath = path.join(htmlDirectory, htmlFile);
+    const screenshotPath = path.join(screenshotDirectory, getScreenshotFileName(htmlFile));
+
+    // Capture screenshot
+    await screenshotWebpage(browser, filePath, screenshotPath);
+  }
+
+  // Close browser
+  await browser.close();
+}
+
+const nodeJsVersion = process.version.match(/(\d+)\.(\d+)\.(\d+)/);
+const [majorVersion, minorVersion, patchVersion] = nodeJsVersion.slice(1).map(_ => parseInt(_));
+
+if (majorVersion < 20 || (majorVersion === 20 && minorVersion < 1)) {
+  throw new Error("NodeJS v20.1.0 is required at minimum. To use the fs.readdir recursive function!");
+}
+
+const screenshotDirectory = path.resolve(process.argv[2]) || path.join(__dirname, 'screenshots/actual');
+const htmlDirectory = path.resolve(process.argv[3]) || path.join(__dirname, '..', 'html');
+main(screenshotDirectory, htmlDirectory);
