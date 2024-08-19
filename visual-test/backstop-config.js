5640aa5c887b7eb7ec074cc9beac5bde88a408a8
Peter-Paul van Gemerden <mail@ppvg.nl>
2024-08-19 16:18:52 +0200

    ci: only generate job summary if ci_report exists

--- visual-test/backstop-config.js
+++ visual-test/backstop-config.js
@@ -29,10 +29,7 @@ export const createBackstopConfig = ({ port, files }) => ({
   report: ["browser", "CI"],
   engine: "puppeteer",
   engineOptions: {
-    args: [
-      "--no-sandbox",
-      "--user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'",
-    ],
+    args: ["--no-sandbox"],
     protocol: "webDriverBiDi",
   },
 });
